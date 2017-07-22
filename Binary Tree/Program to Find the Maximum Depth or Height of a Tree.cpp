ee6109fece906ab3925ece5c37e799f5f6a1637c
Ekansh Gupta <guptaekansh0@gmail.com>
2017-07-22 16:48:02 +0530

    Add files via upload

---
+++ Binary Tree/Program to Find the Maximum Depth or Height of a Tree.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+struct node
+{
+    int data;
+    struct node* left, *right;
+};
+
+struct node* newNode(int data)
+{
+  struct node* node = (struct node*)
+                       malloc(sizeof(struct node));
+  node->data = data;
+  node->left = NULL;
+  node->right = NULL;
+ 
+  return(node);
+}
+
+int max(int a,int b)
+{
+	return a>b?a:b;
+}
+
+int height(node* root)
+{
+	if(root == NULL)
+	{
+		return 0;
+	}
+	return(1+max(height(root->left),height(root->right)));
+}
+
+int main()
+{
+    struct node *root = newNode(1);
+ 
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5); 
+   
+    printf("Hight of tree is %d", height(root));
+   
+    getchar();
+    return 0;
+}
